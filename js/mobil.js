9dfc646ab05a5f750faa1aeb02aa4adb4946d436
makswelll <nikolaevic21.maks@gmail.com>
2023-06-19 23:10:27 +0300

    жесть

---
+++ js/mobil.js
@@ -0,0 +1,18 @@
+const mobileMenu = document.querySelector(".js-menu-container");
+const openMenuBtn = document.querySelector(".js-open-menu");
+const closeMenuBtn = document.querySelector(".js-close-menu");
+
+const toggleMenu = () => {
+  document.body.classList.toggle("no-scroll");
+  mobileMenu.classList.toggle("is-open");
+};
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
+
+// Close the mobile menu on wider screens if the device orientation changes
+window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+  if (!e.matches) return;
+  mobileMenu.classList.remove("is-open");
+  document.body.classList.remove("no-scroll");
+});
